47075239f84a09641899af6da999742ac2974c90
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-06 19:59:06 +0630

    location changed to src/

--- server.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import mongoose from "mongoose";
-
-const app = express();
-const port = 9000;
-
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
